e66be6c663e400ddef448c2aa91c6c5d7567c052
Deepak <grc.deepak@gmail.com>
2016-12-26 16:34:30 -0800

    todo form

--- js/controllers/TodoCtrl.js
+++ js/controllers/TodoCtrl.js
@@ -1,6 +1,15 @@
-toDo.controller('TodoCtrl', ['$scope', function($scope){
+toDo.controller('TodoCtrl', ['$scope', '$window' , function($scope, $window){
   $scope.items = [{ text: "Get groceries from the store", dueDate: new Date(), completed: false },
                   { text: "Make dinner", dueDate: new Date(), completed: false},
                   { text: "Eat dinner", dueDate: new Date(), completed: false}];
+
+  $scope.createTodo = function(valid, form) {
+    if (valid) {
+      $scope.items.push({text: $scope.todoParams.text, dueDate: $scope.todoParams.duedate, completed: false});
+      $scope.todoParams = {};
+      form.$setPristine();
+      form.$setUntouched();
+    }
+  }
 }]
 );
